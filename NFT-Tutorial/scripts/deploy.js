e7080d1a745019bf0399d61ed4ed6aae6e0c9f46
MSBivens <mikebiv95@gmail.com>
2022-06-04 09:28:43 -0500

    built deployment script

---
+++ NFT-Tutorial/scripts/deploy.js
@@ -0,0 +1,20 @@
+// Import ethers from Hardhat package
+const { ethers } = require("hardhat");
+
+async function main() {
+  const nftContract = await ethers.getContractFactory("GameItem");
+
+  // Deploy the contract
+  const deployedNFTContract = await nftContract.deploy();
+
+  // Print the address of the deployed contract
+  console.log("NFT Contract Address:", deployedNFTContract.address);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
